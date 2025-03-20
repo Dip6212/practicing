6ba54607f22d864dddac281120617deda3ad1812
UEM <dipanmukherjee8482@gmail.com>
2025-03-20 14:08:56 +0530

    first commit

---
+++ myProgram.java
@@ -0,0 +1,16 @@
+
+
+public class MyProgram{
+
+    public static void main(String[] args){
+        int a=8;
+        int b=5;
+
+        int temp=a;
+        a=b;
+        b=temp;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
